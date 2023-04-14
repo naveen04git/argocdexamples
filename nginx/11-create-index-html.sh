46fb84abff7bba9d2b7a50a57e011b76e4ab9317
Naveen <naveen@diamanti.com>
2023-04-14 14:53:07 -0700

    Try with 1.3

---
+++ nginx/11-create-index-html.sh
@@ -0,0 +1,11 @@
+echo "<html lang='en'>" > /usr/share/nginx/html/index.html;
+echo "  <head>" >> /usr/share/nginx/html/index.html;
+echo "    <meta charset='utf-8'>" >> /usr/share/nginx/html/index.html;
+echo "    <title>$hostname</title>" >> /usr/share/nginx/html/index.html;
+echo "  </head>" >> /usr/share/nginx/html/index.html;
+echo "  <body>" >> /usr/share/nginx/html/index.html;
+echo "  <font size='8' color='blue'>" >> /usr/share/nginx/html/index.html;
+echo "    $hostname" >> /usr/share/nginx/html/index.html;
+echo " </font>" >> /usr/share/nginx/html/index.html;
+echo "  </body>" >> /usr/share/nginx/html/index.html;
+echo "</html>" >> /usr/share/nginx/html/index.html;
