6736af55c2e7332da2417c49107e4b820e039bbb
Naveen <naveen@diamanti.com>
2023-04-14 15:06:56 -0700

    changed env variable in 1.5

--- nginx/11-create-index-html.sh
+++ nginx/11-create-index-html.sh
@@ -6,7 +6,7 @@ echo "    <title>$hostname</title>" >> /usr/share/nginx/html/index.html;
 echo "  </head>" >> /usr/share/nginx/html/index.html;
 echo "  <body>" >> /usr/share/nginx/html/index.html;
 echo "  <font size='8' color='blue'>" >> /usr/share/nginx/html/index.html;
-echo "    $hostname" >> /usr/share/nginx/html/index.html;
+echo "    $HOSTNAME" >> /usr/share/nginx/html/index.html;
 echo " </font>" >> /usr/share/nginx/html/index.html;
 echo "  </body>" >> /usr/share/nginx/html/index.html;
 echo "</html>" >> /usr/share/nginx/html/index.html;
